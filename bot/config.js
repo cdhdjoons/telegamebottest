e56bf67e04c37ec7b739ec7c597c33efd8fd606e
DevCrypto187 <crypto_dev@marzmain.io>
2025-02-06 22:21:12 +0900

    api test 2

--- bot/config.js
+++ bot/config.js
@@ -1,9 +1,9 @@
-const dotenv = require('dotenv');
-const path = require('path');
+// const dotenv = require('dotenv');
+// const path = require('path');
 
-dotenv.config({ path: path.resolve(__dirname, '../.env.local') });
+// dotenv.config({ path: path.resolve(__dirname, '../.env.local') });
 
-module.exports = {
-  BOT_TOKEN: process.env.BOT_TOKEN,
-  WEBAPP_URL: process.env.WEBAPP_URL
-};
+// module.exports = {
+//   BOT_TOKEN: process.env.BOT_TOKEN,
+//   WEBAPP_URL: process.env.WEBAPP_URL
+// };
